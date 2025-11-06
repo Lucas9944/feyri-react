a8446e3e69ce64ae86cf5229076a47485442f974
lucas9944 <xolmirzayevotabek779@gmail.com>
2025-11-06 09:27:08 +0900

    feat: develop routing based on feyri project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,45 @@
-//import React from "react";
-import { Button, Typography, Container, Stack, Box} from "@mui/material";
-import "../css/App.css"
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Container } from "@mui/material";
+import "../css/App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import { BrandPage } from "../app/screens/BrandPage";
+import { CommunityPage } from "../app/screens/CommunityPage";
+import { OrdersPage } from "../app/screens/OrdersPage";
+import { MemberPage } from "../app/screens/MemberPage";
+import { HelpPage } from "../app/screens/HelpPage";
+import { LoginPage } from "../app/screens/LoginPage";
+import { HomePage } from "../app/screens/HomePage";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack spacing={2} alignItems="center" justifyContent="center" sx={{ height: "100vh" }}>
-        <Typography variant="h4" color="primary">
-          Hello MUI + Redux + TypeScript + Vite 🚀
-        </Typography>
-        <Button variant="contained" color="primary">
-          Bos!
-        </Button>
-      </Stack>
-      <Box>
-        <RippleBadge badgeContent={4}>
-        <Button variant="contained" color="primary">Contained</Button>
-        </RippleBadge>
-      </Box>
+    <Container>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/brand">BrandPage</Link></li>
+              <li><Link to="/community">CommunityPage</Link></li>
+              <li><Link to="/orders">OrdersPage</Link></li>
+              <li><Link to="/member-page">MembersPage</Link></li>
+              <li><Link to="/help">HelpPage</Link></li>
+              <li><Link to="/login">LoginPage</Link></li>
+              <li><Link to="/">HomePage</Link></li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/brand" element={<BrandPage />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/member-page" element={<MemberPage />} />
+            <Route path="/help" element={<HelpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
     </Container>
   );
 }
 
-export default App
+export default App;
---
+++ src/app/screens/BrandPage/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export function BrandPage() {
+  return <Container>BrandPage</Container>;
+}
