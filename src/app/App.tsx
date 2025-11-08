112aff0c346e4df1736e298807857887639a209c
lucas9944 <xolmirzayevotabek779@gmail.com>
2025-11-08 09:33:54 +0900

    feat: develop logic of footers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,59 +1,47 @@
-import { Container } from "@mui/material";
+import React, { useState } from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "../css/App.css";
 import "../css/navbar.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import React, { useState } from "react";
+import "../css/footer.css";
 
-
-import { BrandPage } from "../app/screens/BrandPage";
-import { CommunityPage } from "../app/screens/CommunityPage";
-import { OrdersPage } from "../app/screens/OrdersPage";
-import { MemberPage } from "../app/screens/MemberPage";
-import { HelpPage } from "../app/screens/HelpPage";
-import { LoginPage } from "../app/screens/LoginPage";
-import { HomePage } from "../app/screens/HomePage";
-import { NavbarHome } from "../app/components/header";
-import { NavbarBrand } from "../app/components/header/brand";
-import { NavbarOthers } from "../app/components/header/others";
+import { BrandPage } from "./screens/BrandPage";
+import { CommunityPage } from "./screens/CommunityPage";
+import { OrdersPage } from "./screens/OrdersPage";
+import { MemberPage } from "./screens/MemberPage";
+import { HelpPage } from "./screens/HelpPage";
+import { LoginPage } from "./screens/LoginPage";
+import { HomePage } from "./screens/HomePage";
+import { NavbarHome } from "./components/header";
+import { NavbarBrand } from "./components/header/brand";
+import { NavbarOthers } from "./components/header/others";
+import { Footer } from "../app/components/footer";
 
 function App() {
-  const [path, setPath] = useState();
+  const [path, setPath] = useState<string>();
   const main_path = window.location.pathname;
-  return (
-    <Container>
-      <Router>
-      {main_path == "/" ? (
-          <NavbarHome setPath={setPath} />
-        ) : main_path.includes("/brand") ? (
-          <NavbarBrand setPath={setPath} />
-        ) : (
-          <NavbarOthers setPath={setPath} />
-        )}
-        <div>
-          <nav>
-            <ul>
-              {/* <li><Link to="/brand">BrandPage</Link></li>
-              <li><Link to="/">HomePage</Link></li> */}
-              {/* <li><Link to="/community">CommunityPage</Link></li>
-              <li><Link to="/orders">OrdersPage</Link></li>
-              <li><Link to="/member-page">MembersPage</Link></li>
-              <li><Link to="/help">HelpPage</Link></li>
-              <li><Link to="/login">LoginPage</Link></li> */}
-            </ul>
-          </nav>
 
-          <Routes>
-            <Route path="/brand" element={<BrandPage />} />
-            <Route path="/community" element={<CommunityPage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/member-page" element={<MemberPage />} />
-            <Route path="/help" element={<HelpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </div>
-      </Router>
-    </Container>
+  return (
+    <Router>
+      {main_path === "/" ? (
+        <NavbarHome setPath={setPath} />
+      ) : main_path.includes("/brand") ? (
+        <NavbarBrand setPath={setPath} />
+      ) : (
+        <NavbarOthers setPath={setPath} />
+      )}
+
+      <Routes>
+        <Route path="/brand" element={<BrandPage />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/member-page" element={<MemberPage />} />
+        <Route path="/help" element={<HelpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { Box, Container, Stack } from "@mui/material";
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className="footer_config">
+      <Container>
+        <Stack className="main_footer_container">
+          <Stack flexDirection={"row"} style={{ height: "242px" }}>
+            <Stack className="info" flexDirection={"column"}>
+              <Box className="logo_brand_footer">INSPIRINGSPACES</Box>
+              <Box className="main_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor Sed ut perspiciatis unde omnis iste
+              </Box>
+
+              <Box className="main_text_bold">
+                121 king street, Melbourne 3000 <br />
+                +61 3 8376 6284 <br /> <br />
+                xolmirzayevotabek779@gmail.com
+              </Box>
+              <Stack className="contact_links">
+                <Box>
+                  <img src={"/icons/facebook.svg"} />
+                </Box>
+                <Box>
+                  <img src={"/icons/twitter.svg"} />
+                </Box>
+                <Box>
+                  <img src={"/icons/instagram.svg"} />
+                </Box>
+                <Box>
+                  <img src={"/icons/youtube.svg"} />
+                </Box>
+              </Stack>
+            </Stack>
+            <Stack className="parts">
+              <Box className="part_subject">SHOPPING</Box>
+              <Box className="divider"></Box>
+              <Box className="targets">
+                Your Cart <br /> Your orders Compared items Shipping detail
+              </Box>
+            </Stack>
+            <Stack className="parts">
+              <Box className="part_subject">MORE LINK</Box>
+              <Box className="divider"></Box>
+              <Box className="targets">
+                Blog <br /> Gift Center Buying Guides Clearance
+              </Box>
+            </Stack>
+            <Stack className="wrap_find_us">
+              <Stack className="find_us">
+                <Box className="find">FROM THE BLOG</Box>
+                <Stack className="details" sx={{ mt: "19.36px" }}>
+                  <Box className="detail_first">26 May</Box>
+                  <Box className="detail_second">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit{" "}
+                  </Box>
+                </Stack>
+              </Stack>
+                <Stack className="line_border"></Stack>
+              <Stack className="find_us">
+                <Stack className="details" sx={{ mt: "19.36px" }}>
+                  <Box className="detail_first">27 May</Box>
+                  <Box className="detail_second">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit{" "}
+                  </Box>
+                </Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+      <Stack className="copyrights">
+        Copyright INSPIRINGSPACES 2022, All right reserved
+      </Stack>
+    </div>
+  );
+}
