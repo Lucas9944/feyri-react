ffafa82c4b5614b5ea905f397eb5dc37e0fb05bf
lucas9944 <xolmirzayevotabek779@gmail.com>
2025-11-24 12:45:55 +0900

    ui: finish latest collection product cards and hover actions

--- src/app/screens/HomePage/topRating.tsx
+++ src/app/screens/HomePage/topRating.tsx
@@ -1,345 +1,120 @@
-import { Container, Stack, Box, Button, TextField } from "@mui/material";
-import Card from "@mui/joy/Card";
-import CardCover from "@mui/joy/CardCover";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
-import { CssVarsProvider } from "@mui/joy/styles";
-import { Rating } from "react-simple-star-rating";
-import EmailIcon from "@mui/icons-material/Email";
+// src/app/screens/HomePage/TopRating.tsx
+import { Container, Stack, Box, Typography, Button } from "@mui/material";
+
+type ActionType = "add" | "select" | "buy";
+
+interface Product {
+  id: number;
+  name: string;
+  price: string;
+  image: string;
+  onSale?: boolean;
+  action: ActionType;
+}
+
+const LATEST_PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: "Liquid Gold Midnight Reboot Serum",
+    price: "$230.00",
+    image: "/latest/1.png",
+    action: "add",
+  },
+  {
+    id: 2,
+    name: "Liquid Gold Midnight Reboot Serum",
+    price: "$250.00 – $230.00",
+    image: "/latest/2.png",
+    action: "select",
+  },
+  {
+    id: 3,
+    name: "Vitamin A Serum with 0.5% Retinol",
+    price: "$230.00",
+    image: "/latest/3.png",
+    action: "buy",
+  },
+  {
+    id: 4,
+    name: "Gold Exfoliating Treatment",
+    price: "$230.00",
+    image: "/latest/4.png",
+    action: "add",
+  },
+  {
+    id: 5,
+    name: "Generation Glow Daily",
+    price: "$230.00",
+    image: "/latest/5.png",
+    action: "add",
+  },
+  {
+    id: 6,
+    name: "Liquid Gold Midnight Reboot Serum",
+    price: "$230.00",
+    image: "/latest/6.png",
+    action: "add",
+  },
+  {
+    id: 7,
+    name: "Liquid Gold Midnight Reboot Serum",
+    price: "$230.00",
+    image: "/latest/7.png",
+    action: "buy",
+  },
+  {
+    id: 8,
+    name: "Liquid Gold Midnight Reboot Serum",
+    price: "$200.00  $230.00",
+    image: "/latest/8.png",
+    onSale: true,
+    action: "add",
+  },
+];
+
+function getButtonLabel(action: ActionType) {
+  switch (action) {
+    case "add":
+      return "ADD TO CART ↗";
+    case "select":
+      return "SELECT OPTIONS ↗";
+    case "buy":
+      return "BUY PRODUCT ↗";
+  }
+}
 
 export function TopRating() {
   return (
-    <div className="top_frame">
-      <Container>
-        <Stack
-          flexDirection={"column"}
-          display="flex"
-          alignItems={"center"}
-          justifyContent="space-evenly"
-          style={{ height: "1300px" }}
-        >
-          <Stack className="wrapp_card_joy_rating">
-            <Typography
-              component="h1"
-              style={{
-                fontWeight: "bold",
-                fontSize: "34px",
-                marginTop: "50px",
-                marginLeft: "10px",
-              }}
-            >
-              TOP RATING
-            </Typography>
-            <Stack className="card_joy_rating" direction="row" spacing={2}>
-              <CssVarsProvider>
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/with_food.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/seat.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/glass.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-              </CssVarsProvider>
-            </Stack>
-            <Stack className="card_joy_rating" direction="row" spacing={2}>
-              <CssVarsProvider>
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/with_food.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/seat.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/glass.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-              </CssVarsProvider>
-            </Stack>
-            <Stack className="card_joy_rating" direction="row" spacing={2}>
-              <CssVarsProvider>
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/with_food.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/seat.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-                <Stack
-                  className="wrapp_product_rating"
-                  direction="row"
-                  spacing={2}
-                >
-                  <Card
-                    sx={{
-                      height: "310px",
-                      width: 200,
-                      borderRadius: "none",
-                      border: "none",
-                    }}
-                  >
-                    <CardCover>
-                      <img src="/interior/glass.png" alt="" />
-                    </CardCover>
-                    <CardCover sx={{}} />
-                    <CardContent sx={{ justifyContent: "flex-end" }} />
-                  </Card>
-                  <Box className={"product_price_rating"}>
-                    <p>Linen Beach Towel</p>
-                    <Rating size={25} />
-                    <Typography>30$</Typography>
-                  </Box>
-                </Stack>
-              </CssVarsProvider>
-            </Stack>
-          </Stack>
-          <Button
-            style={{
-              marginBottom: "50px",
-              height: "51px",
-              width: "371px",
-              cursor: "pointer",
-              color: "white", // Tugma rangi
-              backgroundColor: "black", // Qo'shimcha: tugma fon rangi
-              borderRadius: "0px",
-            }}
-          >
-            load more products
-          </Button>
+    <section className="latest_section">
+      <Container maxWidth="lg">
+        <Stack className="latest_header" spacing={1}>
+          <Typography className="latest_title">Latest Collection</Typography>
+          <Typography className="latest_subtitle">
+            Our universally agreed, most-loved products.
+          </Typography>
         </Stack>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent={"space-between"}
-          className="wrapp_input_email"
-        >
-          <Box width="40%">
-            <Typography
-              component="h2"
-              style={{ fontWeight: "bold", fontSize: "28px", color: "black" }}
-            >
-              SIGN UP FOR THE NEWSLETTER
-            </Typography>
 
-            <Typography style={{ fontSize: "18px", color: "black" }}>
-              Subscribe for the latest stories and promotions
-            </Typography>
-          </Box>
-          <Stack
-            direction="row"
-            spacing={2}
-            width="600px"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <TextField
-              placeholder="Enter your e-mail address"
-              variant="outlined"
-              InputProps={{
-                style: {
-                  height: "50px",
-                  width: "517px",
-                  color: "gray",
-                  fontSize: "16px",
-                  borderRadius: "0px",
-                },
-              }}
-            />
-            <Button
-              style={{
-                height: "50px",
-                width: "85px",
-                backgroundColor: "black",
-                color: "white",
-                cursor: "pointer",
-                borderRadius: "0px",
-              }}
-              variant="contained"
-              startIcon={
-                <EmailIcon style={{ color: "white", marginLeft: "10px" }} />
-              }
-            ></Button>
-          </Stack>
-        </Stack>
+        <Box className="latest_grid">
+          {LATEST_PRODUCTS.map((item) => (
+            <article key={item.id} className="latest_card">
+              {/* IVORY PANEL – rasm + button shu yerda */}
+              <div className="latest_media">
+                <div className="latest_image_box">
+                  <img src={item.image} alt={item.name} />
+                </div>
+
+                <Button className="latest_button">
+                  {getButtonLabel(item.action)}
+                </Button>
+              </div>
+
+              {/* text pastda */}
+              <Typography className="latest_name">{item.name}</Typography>
+              <Typography className="latest_price">{item.price}</Typography>
+            </article>
+          ))}
+        </Box>
       </Container>
-    </div>
+    </section>
   );
 }
